6f30453c533c64d9b1dfb1722e55656d0c0f9218
Kayla Plunkett <kayla.lee.plunkett@gmail.com>
2025-05-02 18:06:06 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <>
+      <h1>Hi there, partner</h1>
+      </>
+    )
+  }
+}
+
+export default App
